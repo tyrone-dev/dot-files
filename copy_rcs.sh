1e656d9e183624e4b231f5cdb63dd8bebb0c1d6d
Tyrone van Balla <tvanballa@ska.ac.za>
2016-05-03 07:47:41 +0200

    added bash script to copy dot files

---
+++ copy_rcs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# copies all the rc files in this repo to overwrite the defaults
+
+cp my_vimrc ~/.vimrc
+cp new_bashrc_with_colour ~/.bashrc
+cp my_inputrc ~/.inputrc
+cp my_dircolors ~/.dircolors
