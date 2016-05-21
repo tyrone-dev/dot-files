23eeaa8a8afb5b4f17bed763f0988409aed4e2f2
tyronevb <dev.tyrone@gmail.com>
2016-05-22 00:54:59 +0200

    Add support for git workspaces in bashrc
    Added support for git workspace using cmd. Navigating to git repos now
    show the current branch and commit and indicate if there are changes to
    be commited. This is implemeted by modifying the bashrc files. Old
    my_bashrc also removed.
    
    copy_rcs.sh was also modified to copy the new bash with git support as
    well as two auxilary scripts.

--- copy_rcs.sh
+++ copy_rcs.sh
@@ -3,7 +3,10 @@
 # copies all the rc files in this repo to overwrite the defaults
 
 cp my_vimrc ~/.vimrc
-cp new_bashrc_with_colour ~/.bashrc
+cp git_workspace/bashrc_git ~/.bashrc
+cp git_workspace/git-completion.bash ~/
+cp git_workspace/git-prompt.sh ~/
+# cp new_bashrc_with_colour ~/.bashrc
 cp my_inputrc ~/.inputrc
 cp my_dircolors ~/.dircolors
 cp my_profile ~/.profile
